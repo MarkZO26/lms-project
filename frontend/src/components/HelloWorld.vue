965db963d4c5843192f9a9aeb4bc0db3aaf13ea4
MarCo <markordillano3269@gmail.com>
2026-01-23 16:57:41 +0800

    Update HelloWorld.vue

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -12,6 +12,7 @@ defineProps({
       <div class="nav-links">
         <a href="/login" class="btn-outline">Login</a>
         <a href="/register" class="btn">Register</a>
+        <a href="/I love You" class="btn">I love YOu</a>
       </div>
     </nav>
 
